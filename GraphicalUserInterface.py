289342473b0cf32e95b69d9d945d6c76633c46af
Jarno <jarnovanderwerff@gmail.com>
2024-08-19 09:41:23 +0200

    Interface looks staan. Nu nog de functionaliteit.

--- GraphicalUserInterface.py
+++ GraphicalUserInterface.py
@@ -12,91 +12,114 @@ Wat moet de GUI kunnen?
 import tkinter as tk
 from tkinter import filedialog
 
-def choose_file(entry_widget):
-    # Open file dialog and allow user to select a file
+def choose_file(entry_widget, path_var):
     file_path = filedialog.askopenfilename()
+    if file_path:
+        entry_widget.delete(0, tk.END)
+        entry_widget.insert(0, file_path)
+        path_var.set(file_path)
 
-    # Display the file path in the provided entry widget
-    if file_path:  # Check if a file was selected
-        entry_widget.delete(0, tk.END)  # Clear the entry box
-        entry_widget.insert(0, file_path)  # Insert the file path into the entry box
-
+# Define main window
 root  = tk.Tk()
 root.title('Configuratiescherm')
 root.geometry('1024x768')
 
-starting_text = tk.Label(root,text='Configureer hier hoe je het programma wil draaien', font=('Arial',14))
+# Declare variables used in GUI
+phenotype_path = tk.StringVar()
+genotype_path = tk.StringVar()
+unknowns_correction_path = tk.StringVar()
+customer_data_path = tk.StringVar()
+pdf_check = tk.BooleanVar()
+delete_check = tk.BooleanVar()
+openall_check = tk.BooleanVar()
+
+# Static text in main window
+starting_text = tk.Label(root,
+                         text='Configureer hier hoe je het programma wil draaien',
+                         font=('Arial',14))
 starting_text.grid(row=0,column=0)
 
-phenotype_entry = tk.Entry(root, font=('Arial',14))
-phenotype_entry.grid(row=1,column=0)
+# Choose phenotype file
+phenotype_text = tk.Label(root, text='Fenotype:', font=('Arial',12))
+phenotype_text.grid(row=1,column=0,padx=35,pady=10,sticky='w')
+phenotype_entry = tk.Entry(root,
+                           textvariable=phenotype_path,
+                           font=('Arial',14))
+phenotype_entry.grid(row=1,column=1,padx=10,pady=10,sticky='w')
 choose_phenotype_button = tk.Button(root,
                                     text='Bestand kiezen',
                                     font=('Arial',12),
-                                    command=lambda: choose_file(phenotype_entry)
+                                    command=lambda: choose_file(phenotype_entry, phenotype_path)
                                     )
-choose_phenotype_button.grid(row=1,column=1)
+choose_phenotype_button.grid(row=1,column=2,padx=10,pady=10,sticky='w')
 
-genotype_entry = tk.Entry(root, font=('Arial',14))
-genotype_entry.grid(row=2,column=0)
+# Choose genotype file
+genotype_text = tk.Label(root, text='Genotype:', font=('Arial',12))
+genotype_text.grid(row=2,column=0,padx=35,pady=10,sticky='w')
+genotype_entry = tk.Entry(root,
+                          textvariable=genotype_path,
+                          font=('Arial',14))
+genotype_entry.grid(row=2,column=1,padx=10,pady=10,sticky='w')
 choose_genotype_button = tk.Button(root,
                                    text='Bestand kiezen',
                                    font=('Arial',12),
-                                   command=lambda: choose_file(genotype_entry)
+                                   command=lambda: choose_file(genotype_entry, genotype_path)
                                    )
-choose_genotype_button.grid(row=2,column=1)
+choose_genotype_button.grid(row=2,column=2,padx=10,pady=10,sticky='w')
+
+# Choose corrected unknowns file
+unknowns_text = tk.Label(root, text='Unknowns:', font=('Arial',12))
+unknowns_text.grid(row=3,column=0,padx=35,pady=10,sticky='w')
+unknowns_entry = tk.Entry(root,
+                          textvariable=unknowns_correction_path,
+                          font=('Arial',14))
+unknowns_entry.grid(row=3,column=1,padx=10,pady=10,sticky='w')
+choose_unknowns_button = tk.Button(root,
+                                   text='Bestand kiezen',
+                                   font=('Arial',12),
+                                   command=lambda: choose_file(unknowns_entry, unknowns_correction_path)
+                                   )
+choose_unknowns_button.grid(row=3,column=2,padx=10,pady=10,sticky='w')
+
+# Choose customer_data file
+customer_data_text = tk.Label(root, text='Klantgegevens:', font=('Arial',12))
+customer_data_text.grid(row=4,column=0,padx=35,pady=10,sticky='w')
+customer_data_entry = tk.Entry(root,
+                          textvariable=customer_data_path,
+                          font=('Arial',14))
+customer_data_entry.grid(row=4,column=1,padx=10,pady=10,sticky='w')
+choose_customer_data_button = tk.Button(root,
+                                   text='Bestand kiezen',
+                                   font=('Arial',12),
+                                   command=lambda: choose_file(customer_data_entry, customer_data_path)
+                                   )
+choose_customer_data_button.grid(row=4,column=2,padx=10,pady=10,sticky='w')
+
+# Check generate PDF
+pdf_checkbox = tk.Checkbutton(
+    root
+    ,text='PDF bestanden genereren'
+    ,variable=pdf_check
+    ,font=('Arial',12)
+)
+pdf_checkbox.grid(row=5,column=0,padx=10,pady=10,sticky='w')
+
+# Check delete existing reports
+delete_checkbox = tk.Checkbutton(
+    root
+    ,text='Alle bestaande rapporten verwijderen'
+    ,variable=delete_check
+    ,font=('Arial',12)
+)
+delete_checkbox.grid(row=6,column=0,padx=10,pady=10,sticky='w')
+
+# Check open all reports
+openall_checkbox = tk.Checkbutton(
+    root
+    ,text='Alle rapporten openen'
+    ,variable=openall_check
+    ,font=('Arial',12)
+)
+openall_checkbox.grid(row=7,column=0,padx=10,pady=10,sticky='w')
 
 root.mainloop()
-
-# Variable bruikbaar maken
-"""
-import tkinter as tk
-from tkinter import filedialog
-
-def choose_file(entry_widget, file_var):
-    # Open file dialog and allow user to select a file
-    file_path = filedialog.askopenfilename()
-    
-    # Display the file path in the provided entry widget and store it in the variable
-    if file_path:  # Check if a file was selected
-        entry_widget.delete(0, tk.END)  # Clear the entry box
-        entry_widget.insert(0, file_path)  # Insert the file path into the entry box
-        file_var.set(file_path)  # Store the file path in the variable
-
-# Create the main window
-root = tk.Tk()
-root.title("File Chooser")
-root.geometry("500x200")
-
-# Variables to store the file paths
-file1_path = tk.StringVar()
-file2_path = tk.StringVar()
-
-# Create the first entry widget for the first file
-entry1 = tk.Entry(root, textvariable=file1_path, width=50, font=("Arial", 14))
-entry1.pack(pady=10)
-
-# Create the first button to open the file dialog for the first file
-button1 = tk.Button(root, text="Choose First File", command=lambda: choose_file(entry1, file1_path), font=("Arial", 12))
-button1.pack(pady=5)
-
-# Create the second entry widget for the second file
-entry2 = tk.Entry(root, textvariable=file2_path, width=50, font=("Arial", 14))
-entry2.pack(pady=10)
-
-# Create the second button to open the file dialog for the second file
-button2 = tk.Button(root, text="Choose Second File", command=lambda: choose_file(entry2, file2_path), font=("Arial", 12))
-button2.pack(pady=5)
-
-# Function to display the stored file paths
-def print_file_paths():
-    print(f"First file path: {file1_path.get()}")
-    print(f"Second file path: {file2_path.get()}")
-
-# Add a button to print the stored file paths
-print_button = tk.Button(root, text="Print File Paths", command=print_file_paths, font=("Arial", 12))
-print_button.pack(pady=20)
-
-# Start the Tkinter event loop
-root.mainloop()
-"""
